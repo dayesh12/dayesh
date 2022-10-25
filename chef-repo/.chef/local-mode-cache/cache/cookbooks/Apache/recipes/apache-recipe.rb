bf1a772623e9dc931d6c48fbc8bdf89bd048a593
root <root@ip-172-31-27-27.us-west-1.compute.internal>
2022-10-25 14:49:02 +0000

    chef files

---
+++ chef-repo/.chef/local-mode-cache/cache/cookbooks/Apache/recipes/apache-recipe.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook:: Apache
+# Recipe:: apache-recipe
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#
+package "httpd" do
+  action :install
+end
+file'/var/www/html/index.html' do
+  content 'bro'
+  action :create
+end
+service 'httpd' do
+  action [:enable, :start]
+end
+  
+
+
